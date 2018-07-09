0396df80c0b634e77ce4f9b5d29caa62831f81a6
Musician <musician@abv.bg>
2018-07-09 14:42:16 +0300

    CashIn

--- src/Classes/CashIn/CashIn.php
+++ src/Classes/CashIn/CashIn.php
@@ -19,17 +19,30 @@ class CashIn
      * 
      * $res = 1.75
      */
-    public function comission($amount, $percentage = 0.03, $limit = 5)
+    public function comission($amount, $percentage = 0.03, $limit = 5.00)
     {
-        $fee = ($amount / 100) * $percentage; 
+        $fee = ($amount / 100) * $percentage;
         if ($fee > $limit)
         {
             return $limit;
-        } 
-        else 
+        }
+        else
         {
             return round($fee, 2);
         }
     }
-
+    
+    /**
+     * Alias to method comission, but returns result in monetary formatting. If needed:
+     * setlocale(LC_MONETARY,"en_US");
+     * could be performed, to set the proper currency. 
+     * Another possible usage could be setlocale outside and call "comission()" with money_format() of the returned result. 
+     * I would use this second option with setlocale and money_format outside, but it is good idea to have it, just in case.
+     */
+    public function comission_format($amount, $percentage = 0.03, $limit = 5.00)
+    {
+        $fee = $this->comission($amount, $percentage, $limit);
+        return money_format("%i", $fee);
+    }
+    
 }
--- test.php
+++ test.php
@@ -1,9 +1,14 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
+setlocale(LC_MONETARY,"bg_BG");
+
+
 use Classes\CashIn\CashIn;
 
 $c = new CashIn;
-$res = $c->comission(5843);
+$res = $c->comission(2000.00);
+$res2 = $c->comission_format(2000.00);
 
 echo $res . PHP_EOL;
+echo $res2 . PHP_EOL;
