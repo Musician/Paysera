2dc5dd22b8994a9f7e3d395eba59795614d181c9
Musician <musician@abv.bg>
2018-07-09 14:26:12 +0300

    Easy CashIn finished (calculation of comission)

--- src/Classes/CashIn/CashIn.php
+++ src/Classes/CashIn/CashIn.php
@@ -2,12 +2,34 @@
 
 namespace Classes\CashIn;
 
+/**
+ * Class CashIn
+ * Performs operation on Cash In
+ */
+
 class CashIn
 {
 
-    public function test()
+    /**
+     *  Function comission:
+     *  calculates given percentage for Cashed in amount. 
+     * Usage:
+     * $c = new CashIn;
+     * $res = $c->comission(5843);
+     * 
+     * $res = 1.75
+     */
+    public function comission($amount, $percentage = 0.03, $limit = 5)
     {
-        echo __METHOD__, PHP_EOL;
+        $fee = ($amount / 100) * $percentage; 
+        if ($fee > $limit)
+        {
+            return $limit;
+        } 
+        else 
+        {
+            return round($fee, 2);
+        }
     }
 
 }
--- test.php
+++ test.php
@@ -4,4 +4,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Classes\CashIn\CashIn;
 
 $c = new CashIn;
-$c->test();
+$res = $c->comission(5843);
+
+echo $res . PHP_EOL;
